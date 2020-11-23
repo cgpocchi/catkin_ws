23072cf804e77ee083589fa955c681f798c7f824
cgpocchi <cgpolydor@gmail.com>
2020-11-22 21:48:07 -0500

    first commit

---
+++ build/turtlebot3/turtlebot3_example/CMakeFiles/turtlebot3_example_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/turtlebot3_example_generate_messages_py"
+  "/home/cp/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/_Turtlebot3Goal.py"
+  "/home/cp/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/_Turtlebot3Feedback.py"
+  "/home/cp/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/_Turtlebot3ActionFeedback.py"
+  "/home/cp/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/_Turtlebot3ActionGoal.py"
+  "/home/cp/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/_Turtlebot3Action.py"
+  "/home/cp/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/_Turtlebot3ActionResult.py"
+  "/home/cp/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/_Turtlebot3Result.py"
+  "/home/cp/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/__init__.py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/turtlebot3_example_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
